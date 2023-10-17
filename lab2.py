3e15b11297364dd830aa6979b6be4b0f93a16285
256bit.by <deltas795@gmail.com>
2023-10-17 17:47:45 +0300

    finish second lab

--- lab2.py
+++ lab2.py
@@ -40,36 +40,46 @@ class NeuralNetwork:
         self.y = self.activate()
         return self.y
 
-    def calculate_error(self, inputs: list[float], reference: float) -> float:
-        error = 0
-        for _ in range(len(inputs)):
-            error += (self.y - reference) ** 2
-        return 0.5 * error
+    def calculate_error(self, reference: float) -> float:
+        return 0.5 * ((self.y - reference) ** 2)
 
     def fill_input(self, inputs: list[float]) -> None:
         for neuron, x in zip(self.neurons, inputs):
             neuron.take_input(x)
 
-    def train(self, inputs: list[float], reference: float) -> None:
+    def train(self, inputs: list[float]) -> None:
+        epochs = 0
         optimal = False
         while not optimal:
-            self.calculate_output(inputs)
-            error = self.calculate_error(reference)
+            error = 0
+            print(f'\nЭпоха {epochs + 1}:')
+            for i in range(len(inputs) - 3):
+                input_image = inputs[i:i+3] #от i включительно до i+3 не включительно
+                reference = inputs[i+3]
 
+                self.calculate_output(input_image)
+
+                print(f'Значение: {self.y}\nЭталонное значение: {reference}')
+
+                error += self.calculate_error(reference)
+
+                for neuron in self.neurons:
+                    neuron.weight = neuron.weight - self.a * neuron.x * (self.y - reference)
+                self.t = self.t + self.a * (self.y - reference)
+            epochs += 1
             if error < self.minError:
                 optimal = True
 
-            for neuron in self.neurons:
-                neuron.weight = neuron.weight - self.a * neuron.x * (self.y - reference)
-            self.t = self.t + self.a * (self.y - reference)
-
-    def predict(self, inputs: list[float], duration: int) -> None:
-        window = inputs
+    def predict(self, inputs: list[float], duration: int) -> list[float]:
+        results = []
+        window = [inputs[0], inputs[1], inputs[2]]
         for _ in range(duration):
             buffer_element = self.calculate_output(window)
             for i in range(len(window) - 1):
                 window[i] = window[i + 1]
             window[-1] = buffer_element
+            results.append(window[-1])
+        return results
 
 
 def make_inputs(step: float) -> list[float]:
@@ -82,16 +92,19 @@ def make_inputs(step: float) -> list[float]:
 
 
 # main
-neurones = [Neuron(), Neuron(), Neuron()]
-network = NeuralNetwork(0.1, 0.00001, neurones)
+neurons = [Neuron(), Neuron(), Neuron()]
+network = NeuralNetwork(0.001, 0.000001, neurons)
 
 inputs = make_inputs(0.1)[:30]
-inputs = [inputs[i:i+3] for i in range(27)]
-references = make_inputs(0.1)
+predict_references = make_inputs(0.1)[30:]
 
-for input, reference in zip(inputs, references):
-    network.train(input, reference)
+for i in range(len(inputs)):
+    print(f'Вход {i} = {inputs[i]}')
+for i in range(len(predict_references)):
+    print(f'Эталон {i} = {predict_references[i]}')
 
-network.predict(inputs[-1], 15)
+network.train(inputs)
+results = network.predict(inputs, 15)
 
-
+for i in range(len(results)):
+    print(f'\nСпрогнозированное значение: {results[i]}\nЭталонное значение: {inputs[i+3]}')
