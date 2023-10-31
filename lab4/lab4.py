7a98af5be4469550c486dfdf426ed1f70bd9a620
256bit.by <deltas795@gmail.com>
2023-11-01 01:04:08 +0300

    Starting make 4 lab

---
+++ lab4/lab4.py
@@ -0,0 +1,35 @@
+# Лабораторная работа №4
+# Вариант 4: y = 0.4 * cos(0.4 * x) + 0.08 * sin(0.4 * x)
+
+from math import sin
+from neuroarchitecture import Neuron, NeuralNetwork
+
+
+# function for calculate values from sinus-function
+def make_inputs(step: float) -> list[float]:
+    sinuses = []
+    x = 0
+    for _ in range(45):
+        #sinuses.append(0.4 * sin(0.4 * x) + 0.4)
+        x += step
+    return sinuses
+
+
+# main
+neurons_num = 3
+min_error = 0.0000001
+
+network = NeuralNetwork()
+for _ in range(neurons_num):
+    network.add_neuron(Neuron())
+
+# :30 означает взять с 0 по 29 из списка, возвращаемого make_inputs(), 30: означает с 30 и до конца
+inputs = make_inputs(0.1)[:30]
+predict_references = make_inputs(0.1)[30:]
+
+network.train(inputs, min_error)
+results = network.predict(inputs, 15)
+
+for i in range(len(results)):
+    print(f'\nСпрогнозированное значение: {results[i]}\nЭталонное значение: {inputs[i+3]}'
+          f'\nСреднеквадратичное отклонение: {0.5 * ((results[i] - inputs[i + 3]) ** 2)}')
---
+++ lab4/neuroarchitecture.py
@@ -0,0 +1,46 @@
+from random import random
+
+
+class Neuron:
+    def __init__(self, outputs_num: int):
+        self.weights = [random() for _ in range(outputs_num)]
+        self.x = 0
+
+    def take_input(self, x: float):
+        self.x = x
+
+    # activate-here
+
+
+class NeuronLayer:
+    def __init__(self, input_neuron_num: int, output_neuron_num=None):
+        self.t = random()
+
+        if output_neuron_num is None:
+            output_neuron_num = input_neuron_num
+
+        self.input_neurons = [Neuron(output_neuron_num) for _ in range(input_neuron_num)]
+        self.outputs = [0 for _ in range(output_neuron_num)]
+
+    def fill_inputs(self, input_image: list[float]) -> None:
+        for neuron, x in zip(self.input_neurons, input_image):
+            neuron.take_input(x)
+
+    def make_outputs(self) -> list[float]:
+        self.outputs = [0 for _ in range(len(self.outputs))]
+
+        for j in range(len(self.outputs)):
+            for i in range(len(self.input_neurons)):
+                self.outputs[j] += self.input_neurons[i].weights[j] * self.input_neurons[i].x
+            self.outputs[j] -= self.t
+        return self.outputs
+
+
+class NeuralNetwork:
+    def __init__(self, layers_config: list[int]):
+        self.layers = [NeuronLayer(layers_config[i]) for i in range(len(layers_config))]
+
+    def make_result(self, input_image: list[float]):
+        self.layers[0].fill_inputs(input_image)
+        for i in range(len(self.layers) - 1):
+            self.layers[i + 1].fill_inputs(self.layers[i].make_outputs())
